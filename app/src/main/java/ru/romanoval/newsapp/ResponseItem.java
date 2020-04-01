3ae81d0f03c96d32c4a59154d298dcceb59731f1
romanovAl <ml.naslednik2001@gmail.com>
2020-04-01 16:44:34 +0500

    Начата работа йо

---
+++ app/src/main/java/ru/romanoval/newsapp/ResponseItem.java
@@ -0,0 +1,33 @@
+package ru.romanoval.newsapp;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ResponseItem {
+
+    @SerializedName("totalResults")
+    @Expose
+    private Integer totalResults;
+    @SerializedName("articles")
+    @Expose
+    private List<Article> articles = null;
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+}
